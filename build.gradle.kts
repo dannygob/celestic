8c6a2f7ff0c7e8b15ef895c43fe473b54506456a
Superhola <68669782+dannygob@users.noreply.github.com>
2025-04-20 16:50:05 +0200

    change gradle proyect

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,25 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.compose) apply false
+    id("com.android.application") version "8.1.4" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
 }
+
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "celestica"
+include(":app")
