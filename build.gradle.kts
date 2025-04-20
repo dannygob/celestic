d4ad915251906fcbaf76af708c7d568a24b755c6
Tu Nombre <computadoraesp@gmail.com>
2025-04-20 17:07:11 +0200

    change toml

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
     id("com.android.application") version "8.1.4" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
 }
 
 pluginManagement {
